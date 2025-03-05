95527427312834312809b09652df77a441997c5b
Meghana Mudduluru <muddulurumeghana123@gmail.com>
2025-03-05 15:26:32 +0530

    day23

---
+++ leetcode/count_total_no_of_coloured_cells.py
@@ -0,0 +1,19 @@
+class Solution:
+    def coloredCells(self, n: int) -> int:
+        num_blue_cells = 1
+        addend = 4
+
+        # Iterate n - 1 times
+        while n - 1:
+
+            # Add and update current multiple of 4
+            num_blue_cells += addend
+            addend += 4
+            n -= 1
+
+        return num_blue_cells
+
+n=Solution()
+print(n.coloredCells(1))
+print(n.coloredCells(2))
+print(n.coloredCells(3))
