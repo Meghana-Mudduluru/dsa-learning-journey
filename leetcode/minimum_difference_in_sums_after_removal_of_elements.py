06ca4415ae0615d9a5c56c002fd33b709dd81484
Meghana Mudduluru <muddulurumeghana123@gmail.com>
2025-07-22 20:24:24 +0530

    day158

---
+++ leetcode/minimum_difference_in_sums_after_removal_of_elements.py
@@ -0,0 +1,75 @@
+'''
+You are given a 0-indexed integer array nums consisting of 3 * n elements.
+
+You are allowed to remove any subsequence of elements of size exactly n from nums. The remaining 2 * n elements will be divided into two equal parts:
+
+    The first n elements belonging to the first part and their sum is sumfirst.
+    The next n elements belonging to the second part and their sum is sumsecond.
+
+The difference in sums of the two parts is denoted as sumfirst - sumsecond.
+
+    For example, if sumfirst = 3 and sumsecond = 2, their difference is 1.
+    Similarly, if sumfirst = 2 and sumsecond = 3, their difference is -1.
+
+Return the minimum difference possible between the sums of the two parts after the removal of n elements.
+
+
+
+Example 1:
+
+Input: nums = [3,1,2]
+Output: -1
+Explanation: Here, nums has 3 elements, so n = 1.
+Thus we have to remove 1 element from nums and divide the array into two equal parts.
+- If we remove nums[0] = 3, the array will be [1,2]. The difference in sums of the two parts will be 1 - 2 = -1.
+- If we remove nums[1] = 1, the array will be [3,2]. The difference in sums of the two parts will be 3 - 2 = 1.
+- If we remove nums[2] = 2, the array will be [3,1]. The difference in sums of the two parts will be 3 - 1 = 2.
+The minimum difference between sums of the two parts is min(-1,1,2) = -1.
+
+Example 2:
+
+Input: nums = [7,9,5,8,1,3]
+Output: 1
+Explanation: Here n = 2. So we must remove 2 elements and divide the remaining array into two parts containing two elements each.
+If we remove nums[2] = 5 and nums[3] = 8, the resultant array will be [7,9,1,3]. The difference in sums will be (7+9) - (1+3) = 12.
+To obtain the minimum difference, we should remove nums[1] = 9 and nums[4] = 1. The resultant array becomes [7,5,8,3]. The difference in sums of the two parts is (7+5) - (8+3) = 1.
+It can be shown that it is not possible to obtain a difference smaller than 1.
+
+'''
+import heapq
+from typing import List
+class Solution:
+    def minimumDifference(self, nums: List[int]) -> int:
+        n3, n = len(nums), len(nums) // 3
+
+        part1 = [0] * (n + 1)
+        # max heap
+        total = sum(nums[:n])
+        ql = [-x for x in nums[:n]]
+        heapq.heapify(ql)
+        part1[0] = total
+
+        for i in range(n, n * 2):
+            total += nums[i]
+            heapq.heappush(ql, -nums[i])
+            total -= -heapq.heappop(ql)
+            part1[i - (n - 1)] = total
+
+        # min heap
+        part2 = sum(nums[n * 2 :])
+        qr = nums[n * 2 :]
+        heapq.heapify(qr)
+        ans = part1[n] - part2
+
+        for i in range(n * 2 - 1, n - 1, -1):
+            part2 += nums[i]
+            heapq.heappush(qr, nums[i])
+            part2 -= heapq.heappop(qr)
+            ans = min(ans, part1[i - n] - part2)
+
+        return ans
+
+nums = [3,1,2]
+
+sol=Solution()
+print(sol.minimumDifference(nums))
