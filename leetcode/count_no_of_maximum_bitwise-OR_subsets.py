024c6a5f96d052547cb2ba510689042a1535d852
Meghana Mudduluru <muddulurumeghana123@gmail.com>
2025-07-28 22:32:46 +0530

    day168

---
+++ leetcode/count_no_of_maximum_bitwise-OR_subsets.py
@@ -0,0 +1,57 @@
+'''
+Given an integer array nums, find the maximum possible bitwise OR of a subset of nums and return the number of different non-empty subsets with the maximum bitwise OR.
+
+An array a is a subset of an array b if a can be obtained from b by deleting some (possibly zero) elements of b. Two subsets are considered different if the indices of the elements chosen are different.
+
+The bitwise OR of an array a is equal to a[0] OR a[1] OR ... OR a[a.length - 1] (0-indexed).
+
+
+
+Example 1:
+
+Input: nums = [3,1]
+Output: 2
+Explanation: The maximum possible bitwise OR of a subset is 3. There are 2 subsets with a bitwise OR of 3:
+- [3]
+- [3,1]
+
+Example 2:
+
+Input: nums = [2,2,2]
+Output: 7
+Explanation: All non-empty subsets of [2,2,2] have a bitwise OR of 2. There are 23 - 1 = 7 total subsets.
+
+Example 3:
+
+Input: nums = [3,2,1,5]
+Output: 6
+Explanation: The maximum possible bitwise OR of a subset is 7. There are 6 subsets with a bitwise OR of 7:
+- [3,5]
+- [3,1,5]
+- [3,2,5]
+- [3,2,1,5]
+- [2,5]
+- [2,1,5]
+'''
+
+from typing import List
+class Solution:
+    def countMaxOrSubsets(self, nums: List[int]) -> int:
+        max_or = 0
+        for num in nums:
+            max_or |= num
+        return self._dfs(nums, 0, 0, max_or)
+
+    def _dfs(self, nums: List[int], i: int, current_or: int, target: int) -> int:
+        if i == len(nums):
+            return 1 if current_or == target else 0
+
+        include = self._dfs(nums, i + 1, current_or | nums[i], target)
+        exclude = self._dfs(nums, i + 1, current_or, target)
+
+        return include + exclude
+
+nums = [3,2,1,5]
+
+sol=Solution()
+print(sol.countMaxOrSubsets(nums))
